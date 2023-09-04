a922c67af11b9fdc6f3e60f7d82c2f5671764df4
MSK4477 <ks7997067@gmail.com>
2023-09-04 07:35:05 +0530

    added changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express"
 import DbToConnect from "./db_utils/dbConnect.js"
-import cors from "cors"
+import cors from "cors"   
 import userRouter from "./router/user.js"
  const app = express()
  app.use(cors())
